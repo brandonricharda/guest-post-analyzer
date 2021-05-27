c5a7b7cb0418daf15e145c62c17da4fb072be367
brandonricharda <brandonricharda@gmail.com>
2021-05-26 21:52:26 -0400

    Add initial Batches#show page design

--- app/models/guest_post.rb
+++ app/models/guest_post.rb
@@ -11,7 +11,7 @@ class GuestPost < ApplicationRecord
         results = {}
 
         article = session.file_by_url(self.url)
-        
+
         html = article.export_as_string("html")
         parsable_article = Nokogiri::HTML.fragment(html)
 
@@ -22,7 +22,7 @@ class GuestPost < ApplicationRecord
             clean_url = raw_url.split("/url?q=")[1].split("&")[0]
             results[link.text] = clean_url
         end
-
+    
         results
 
     end
